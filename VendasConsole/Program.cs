7160dce0cf6f1ace81d60c8747537f1d6ea588f6
Vinicius Lara <viniciusfelipe501@hotmail.com>
2020-08-12 10:20:17 -0300

    Adição do menu

--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -6,7 +6,38 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" -------- Vendas --------\n");
+                Console.WriteLine("1 - Cadastrar Clientes");
+                Console.WriteLine("2 - Listar Clientes");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("\nDigite a opção desejada:");
+                opcao = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine(" -------- Cadastro de Clientes --------\n");
+                        break;
+                    case 2:
+                        Console.WriteLine(" -------- Lista de Clientes --------\n");
+                        break;
+                    case 0:
+                        Console.WriteLine("Saindo...\n");
+                        break;
+                    default:
+                        Console.WriteLine("-------- CÓDIGO INVÁLIDO--------\n");
+                        break;
+                }
+
+                Console.WriteLine("\nPressione uma tecla para continuar...");
+                Console.ReadKey();
+
+            } while (opcao != 0);
         }
     }
 }
